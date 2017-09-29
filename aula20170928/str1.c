7938827813a075f8379e3b8c8e3d9513cf8b62a6
alanamiyai <alana@bonitatibus.com.br>
2017-09-28 21:27:38 -0300

    fim de aula

--- aula20170928/str1.c
+++ aula20170928/str1.c
@@ -4,20 +4,19 @@
 using namespace std;
 void main ()
 {
-	char frase[10000], str1[]= "bom dia", str2[] = "tchau";
+	char frase[10000];
 	int ret1, ret2;
 	printf("Escreva uma frase:\n");
 	fgets(frase,10000,stdin);
+	frase[strlen (frase)-1]='\0';
 	for(int i=0;frase[i]!='\0';i++)
 		frase[i]=(tolower(frase[i]));
 	printf("A string %s possui %d caracteres\n", frase, strlen(frase));
-	ret1 = strcmp(frase, str1);
-	if(ret1==0) 
-        printf("\Bom Dia pra voce tambem!"); 
+	if (strncmp (frase, "bom dia", 7)==0)
+        printf("\Bom Dia pra voce tambem!\n"); 
     else
         printf("\nVoce quis dizer: %s ? \n\n", frase);
-	ret2= strcmp(frase, str2);
-	if(ret2==0)
+	if (strncmp (frase, "tchau", 5)==0)
 		printf("\nSaindo? Que pena...\n\n");
 	else
 		printf(" ");
---
+++ aula20170928/str3.c
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+void main ()
+{
+	int i;
+	char frase[1000];
+	char cifra[1000];
+	printf ("\nEntre com uma frase: \n");
+	fgets(frase, 1000, stdin);
+	frase [strlen (frase)-1]='\0';
+	for(i=0; frase[i]!='\0'; i++)
+	{
+		if(isalpha(frase[i]))
+		{
+			if ((frase[i] >=65 && frase[i]<78)||(frase[i]>97 && frase[i]<110))
+				cifra[i]=frase[i] +13;
+			else
+				cifra[i]=frase[i]-13;
+		}
+		else
+			cifra[i]=frase[i];
+	}
+	printf("\nMensagem: \n %s \n", cifra);
+	system ("pause");
+}
