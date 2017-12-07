6615d73f97841c4da5b86f4d43632313c7416107
alanamiyai <31655913+alanamiyai@users.noreply.github.com>
2017-12-07 10:13:13 -0200

    fim de aula

--- aula20170921/iif3.c
+++ aula20170921/iif3.c
@@ -1,17 +1,20 @@
-﻿'iif3.exe': Loaded 'C:\Users\online\Documents\Visual Studio 2010\Projects\iif3\Release\iif3.exe', Symbols loaded.
-'iif3.exe': Loaded 'C:\Windows\SysWOW64\ntdll.dll', Cannot find or open the PDB file
-'iif3.exe': Loaded 'C:\Windows\SysWOW64\kernel32.dll', Cannot find or open the PDB file
-'iif3.exe': Unloaded 'C:\Windows\SysWOW64\kernel32.dll'
-'iif3.exe': Loaded 'C:\Windows\SysWOW64\kernel32.dll', Cannot find or open the PDB file
-'iif3.exe': Loaded 'C:\Windows\SysWOW64\KernelBase.dll', Cannot find or open the PDB file
-'iif3.exe': Loaded 'C:\Program Files\AVAST Software\Avast\aswhookx.dll', Cannot find or open the PDB file
-'iif3.exe': Loaded 'C:\Windows\SysWOW64\msvcr100.dll', Cannot find or open the PDB file
-'iif3.exe': Loaded 'C:\Windows\SysWOW64\advapi32.dll', Cannot find or open the PDB file
-'iif3.exe': Loaded 'C:\Windows\SysWOW64\msvcrt.dll', Cannot find or open the PDB file
-'iif3.exe': Loaded 'C:\Windows\SysWOW64\sechost.dll', Cannot find or open the PDB file
-'iif3.exe': Loaded 'C:\Windows\SysWOW64\rpcrt4.dll', Cannot find or open the PDB file
-'iif3.exe': Loaded 'C:\Windows\SysWOW64\sspicli.dll', Cannot find or open the PDB file
-'iif3.exe': Loaded 'C:\Windows\SysWOW64\cryptbase.dll', Cannot find or open the PDB file
-'iif3.exe': Loaded 'C:\Windows\SysWOW64\bcryptprimitives.dll', Cannot find or open the PDB file
-The thread 'Win32 Thread' (0x2668) has exited with code -1073741510 (0xc000013a).
-The program '[12692] iif3.exe: Native' has exited with code -1073741510 (0xc000013a).
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	double fat, n;
+	printf("Insira um valor para o qual deseja calcular seu fatorial: ");
+	scanf("%lf", &n);
+        if (n <= 20)
+        {
+	for(fat = 1; n > 1; n = n - 1)
+		fat = fat * n;
+	printf("\nFatorial calculado: %lf", fat);
+        }
+        else
+        printf("Dados invalidos!");
+	getche ();
+  return 0;
+}
