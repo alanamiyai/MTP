fe8a3686876562bfbbbd90701be31ffa71771902
alanamiyai <31655913+alanamiyai@users.noreply.github.com>
2017-12-07 09:58:22 -0200

    fim de aula

--- aula20170905/seq1.c
+++ aula20170905/seq1.c
@@ -1,13 +1,13 @@
-SEQL.C
 #include <iostream>
 #include <stdio.h>
-using namespace std;
-void SafeFlush ()
+#include <conio.h>
+int main ()
 {
 	int x, y;
 	printf("Digite um numero para ser calculado o seu quadrado: \n");
 	scanf("%d",&x);
 	y=x*x;
 	printf("Quadrado= %d",&y"\n");
-	system("pause");
+	getche ();
+	return 0;
 }
