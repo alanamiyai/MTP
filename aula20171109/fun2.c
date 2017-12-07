292244442c0b68d79776b6331cf1a4cd6ea9959c
alanamiyai <31655913+alanamiyai@users.noreply.github.com>
2017-12-07 11:02:06 -0200

    fim de aula

--- aula20171109/fun2.c
+++ aula20171109/fun2.c
@@ -1,13 +1,11 @@
-#include <iostream>
 #include <stdio.h>
-using namespace std;
 void divisao (int *x, int *y, int *z, int *w)
 {
 	*z=*x/ *y;
 	*w= *x%*y;
 	printf("\nQuociente = %d. Resto = %d\n\n", *z, *w);
 }
-void main ()
+int main ()
 {
 	int x, y, z, w;
 	printf("Digite um numero inteiro: \n");
@@ -15,6 +13,6 @@ void main ()
 	printf("Digite outro numero inteiro: \n");
 	scanf("%d", &y);
 	divisao(&x, &y, &z, &w);
-	system("pause");
+	return 0;
 }
 
